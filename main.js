5fa548f46df47435b3b579c7a1b39ea0ad5612fe
Camila Muzel <camila.muzel@unesp.br>
2026-08-14 22:16:52 -0300

    ajustes HTML

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const botaoAdicao = document.getElementById ('adicao')
 
 function somar () {
     const num1 = Number(document.getElementById ('numero1').value)
-    const num2 = Number(document.getElementById ('numero2'))
+    const num2 = Number(document.getElementById ('numero2').value)
 
     const resultado = document.getElementById ('resultado')
 
